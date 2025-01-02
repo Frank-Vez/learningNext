01b920dacef07e624a71a9ccd0a0f51f8ce146bf
Frank-Vez <vezina.francis.04@gmail.com>
2025-01-02 15:09:21 -0500

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Customer = () => {
+  return (
+    <div>
+      <p>This is the customer page</p>
+    </div>
+  );
+};
+
+export default Customer;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Invoices = () => {
+  return (
+    <div>
+      <p>This is the invoices page</p>
+    </div>
+  );
+};
+
+export default Invoices;
